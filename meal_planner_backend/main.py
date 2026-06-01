6bc78f66ad684d77bebf5d8ecc6d8f21d7c2eb2e
CheckToSee <138322844+CheckToSee@users.noreply.github.com>
2026-06-01 16:19:28 -0400

    Finished landing page prototype. Start on preferences page.

--- meal_planner_backend/main.py
+++ meal_planner_backend/main.py
@@ -1,9 +1,17 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 from dotenv import load_dotenv
 from routers import stores, mealplan
 
 load_dotenv()
 app = FastAPI(title="Meal Planner API")
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["http://localhost:5173"],
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 app.include_router(stores.router, prefix="/stores", tags=["stores"])
 app.include_router(mealplan.router, prefix="/mealplan", tags=["mealplan"])
